e87b708ea0e6011041ed1c0a22a0ff4021481d2f
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2025-12-16 22:24:46 +0100

    refactor(web): update routing to user profile pages

--- frontend/FleetForge/src/app/app.routes.ts
+++ frontend/FleetForge/src/app/app.routes.ts
@@ -4,11 +4,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { UserProfileDriverComponent } from './profiles/user-profile-driver/user-profile-driver.component';
+import { UserProfileComponent } from './profiles/user-profile/user-profile.component';
+import { PasswordResetComponent } from './profiles/password-reset/password-reset.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+   {path:'profile',component:UserProfileComponent},
+  {path:'profile-driver',component:UserProfileDriverComponent},
+  {path:'profile-password-reset',component:PasswordResetComponent}
 ];
--- frontend/FleetForge/src/app/auth/login/login.component.ts
+++ frontend/FleetForge/src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { LogoComponent } from '../../shared/logo/logo.component';
+import { UserService } from '../../profiles/service/user-service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,14 @@ import { LogoComponent } from '../../shared/logo/logo.component';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  goToProfile(): void {
+    const user = this.userService.user();
+    if (user?.id === 2) {
+      this.router.navigate(['profile-driver']);
+    } else {
+      this.router.navigate(['profile']);
+    }
+  }
 }
