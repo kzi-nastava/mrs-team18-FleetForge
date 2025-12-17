ea0e9751ca5ba71ba98d72eededeb8cf04a8ba5f
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-12-17 01:27:47 +0100

    feat(web): Hide navbar on auth routes using layouts

--- frontend/FleetForge/src/app/app.routes.ts
+++ frontend/FleetForge/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+
+import { MainLayoutComponent } from './layout/main/main.layout';
+import { AuthLayoutComponent } from './layout/auth/auth.layout';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -9,12 +13,27 @@ import { UserProfileComponent } from './profiles/user-profile/user-profile.compo
 import { PasswordResetComponent } from './profiles/password-reset/password-reset.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-   {path:'profile',component:UserProfileComponent},
-  {path:'profile-driver',component:UserProfileDriverComponent},
-  {path:'profile-password-reset',component:PasswordResetComponent}
+  /** ROUTES WITH NAVBAR */
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'profile', component: UserProfileComponent },
+      { path: 'profile-driver', component: UserProfileDriverComponent },
+      { path: 'profile-password-reset', component: PasswordResetComponent }
+    ]
+  },
+
+  /** ROUTES WITHOUT NAVBAR (AUTH) */
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'reset-password', component: ResetPasswordComponent }
+    ]
+  }
 ];
