27634084972e2d1e7c4ae9e51f9161165ef8189a
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-12-25 02:43:19 +0100

    chore(api): Restart project using Spring Initializr

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/FleetForgeApplication.java
@@ -0,0 +1,13 @@
+package com.team18.FleetForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetForgeApplication.class, args);
+	}
+
+}
---
+++ backend/FleetForge/src/test/java/com/team18/FleetForge/FleetForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team18.FleetForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
