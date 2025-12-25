025e46195cfd957df0b3691e6c4ae3b16e9003ae
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2025-12-25 21:01:03 +0100

    feat(backend): added controler for driver with one method for creating driver and one method for setting drivers password

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/controller/DriverControler.java
@@ -0,0 +1,48 @@
+package com.team18.FleetForge.controller;
+
+import com.team18.FleetForge.dto.*;
+import com.team18.FleetForge.model.Driver;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/driver")
+public class DriverControler {
+
+    @PostMapping
+    public ResponseEntity<DriverCreateResponseDTO> createDriver(@RequestBody DriverCreateRequestDTO request){
+        DriverCreateResponseDTO driverCreateResponseDTO = new DriverCreateResponseDTO();
+        driverCreateResponseDTO.setFirstName(request.getFirstName());
+        driverCreateResponseDTO.setLastName(request.getLastName());
+        driverCreateResponseDTO.setEmail(request.getEmail());
+        driverCreateResponseDTO.setPhone(request.getPhone());
+        driverCreateResponseDTO.setAddress(request.getAddress());
+
+        VehicleCreateResponseDTO vehicleCreateResponseDTO = new VehicleCreateResponseDTO();
+        vehicleCreateResponseDTO.setType(request.getVehicle().getType());
+        vehicleCreateResponseDTO.setRegistrationNumber(request.getVehicle().getRegistrationNumber());
+        vehicleCreateResponseDTO.setModel(request.getVehicle().getModel());
+        vehicleCreateResponseDTO.setSpace(request.getVehicle().getSpace());
+        vehicleCreateResponseDTO.setPetFriendly(request.getVehicle().isPetFriendly());
+        vehicleCreateResponseDTO.setBabySeat(request.getVehicle().isBabySeat());
+
+        driverCreateResponseDTO.setVehicle(vehicleCreateResponseDTO);
+    //treba sacuvati sve posle u bazi
+        return  new ResponseEntity<>(driverCreateResponseDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}/set-password")
+    public ResponseEntity<DriverPasswordResponseDTO> setDriverPassword(@PathVariable long id, @RequestBody DriverPasswordRequestDTO request){
+        DriverPasswordResponseDTO driverPasswordResponseDTO = new DriverPasswordResponseDTO();
+
+        //ovde treba da se dobavlja iz baze preko id pa da se azurira i da se vrati nesto ovo sam lupio podatke samo sad jer nema baze
+        driverPasswordResponseDTO.setId(id);
+        driverPasswordResponseDTO.setEmail("milos.damjanovic@gmail.com");
+
+        return new ResponseEntity<>(driverPasswordResponseDTO, HttpStatus.OK);
+    }
+
+
+}
