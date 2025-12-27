91c63e24445a71d0ee1234490e36476a4378fef7
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2025-12-27 13:21:07 +0100

    feat(api): Add controller class for passenger

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/controller/PassengerController.java
@@ -0,0 +1,52 @@
+package com.team18.FleetForge.controller;
+
+import com.team18.FleetForge.dto.driver.DriverGetResponseDTO;
+import com.team18.FleetForge.dto.passenger.PassengerChangeInformationRequestDTO;
+import com.team18.FleetForge.dto.passenger.PassengerChangeInformationResponseDTO;
+import com.team18.FleetForge.dto.passenger.PassengerGetResponseDTO;
+import com.team18.FleetForge.model.Users.Driver;
+import com.team18.FleetForge.model.Users.Passenger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/passenger")
+public class PassengerController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PassengerGetResponseDTO> getDriver(@PathVariable Long id) {
+        Passenger foundPassenger =new Passenger();
+        foundPassenger.setId(id);
+        foundPassenger.setFirstName("Admin");
+        foundPassenger.setLastName("Admin");
+        foundPassenger.setPassword("admin");
+        foundPassenger.setEmail("admin");
+        foundPassenger.setPhoneNumber("123456789");
+        foundPassenger.setAddress("address");
+        foundPassenger.setProfilePicture("profilePicture");
+
+        PassengerGetResponseDTO passengerGetResponseDTO = new PassengerGetResponseDTO(foundPassenger);
+
+        return new ResponseEntity<>(passengerGetResponseDTO, HttpStatus.OK);
+    }
+    @PutMapping("/update-passenger/{id}")
+    public ResponseEntity<PassengerChangeInformationResponseDTO> changeUser
+            (@RequestBody PassengerChangeInformationRequestDTO passengerChangeInformationRequestDTO, @PathVariable String id) {
+        //prvo ga posle trazim preko id i postavim nove podatke
+        Passenger passengerToChange = Passenger.builder()
+                .firstName(passengerChangeInformationRequestDTO.getFirstName())
+                .lastName(passengerChangeInformationRequestDTO.getLastName())
+                .email(passengerChangeInformationRequestDTO.getEmail())
+                .phoneNumber(passengerChangeInformationRequestDTO.getPhoneNumber())
+                .address(passengerChangeInformationRequestDTO.getAddress())
+                .profilePicture(passengerChangeInformationRequestDTO.getProfilePicture())
+                .build();
+
+        PassengerChangeInformationResponseDTO passengerChanged = new PassengerChangeInformationResponseDTO(passengerToChange);
+        return new ResponseEntity<>(passengerChanged, HttpStatus.OK);
+
+    }
+
+
+}
