5bd6e33d6f63d44fd195708b0e0e9790aa6d362e
Milos Damjanovic <mdamjanovic2004@gmail.com>
2025-12-27 12:48:04 +0100

    Add DTO class for unregistered view information endpoint

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/dto/vehicle/VehicleLocationDTO.java
@@ -0,0 +1,19 @@
+package com.team18.FleetForge.dto.vehicle;
+
+import com.team18.FleetForge.model.GeoPoint;
+import com.team18.FleetForge.utils.VehicleType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleLocationDTO {
+    private Long vehicleId;
+    private String model;
+    private VehicleType vehicleType;
+    private GeoPoint currentLocation;
+    private Boolean isAvailable;
+    private Boolean isActive;
+}
