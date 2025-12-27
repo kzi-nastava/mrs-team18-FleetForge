7362082342fb96fd0637aacef93704a8df9a3539
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2025-12-27 13:24:32 +0100

    feat(api): Add endpoints for creating change request and returning driver by id

--- backend/FleetForge/src/main/java/com/team18/FleetForge/controller/DriverControler.java
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/controller/DriverControler.java
@@ -1,17 +1,36 @@
 package com.team18.FleetForge.controller;
 
-import com.team18.FleetForge.dto.driver.DriverCreateRequestDTO;
-import com.team18.FleetForge.dto.driver.DriverCreateResponseDTO;
-import com.team18.FleetForge.dto.driver.DriverPasswordRequestDTO;
-import com.team18.FleetForge.dto.driver.DriverPasswordResponseDTO;
+import com.team18.FleetForge.dto.driver.*;
 import com.team18.FleetForge.dto.vehicle.VehicleCreateResponseDTO;
+import com.team18.FleetForge.model.DriverProfileChangeRequest;
+import com.team18.FleetForge.model.Users.Driver;
+import com.team18.FleetForge.model.enums.DriverProfileChangeRequestStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
-@RequestMapping("/admin/driver")
+@RequestMapping("/api/driver")
 public class DriverControler {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DriverGetResponseDTO> getDriver(@PathVariable Long id) {
+        Driver foundDriver =new Driver();
+        foundDriver.setId(id);
+        foundDriver.setFirstName("Admin");
+        foundDriver.setLastName("Admin");
+        foundDriver.setPassword("admin");
+        foundDriver.setEmail("admin");
+        foundDriver.setPhoneNumber("123456789");
+        foundDriver.setAddress("address");
+        foundDriver.setProfilePicture("profilePicture");
+
+        DriverGetResponseDTO driverGetResponseDTO = new DriverGetResponseDTO(foundDriver);
+
+        return new ResponseEntity<>(driverGetResponseDTO, HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<DriverCreateResponseDTO> createDriver(@RequestBody DriverCreateRequestDTO request){
@@ -46,5 +65,26 @@ public class DriverControler {
         return new ResponseEntity<>(driverPasswordResponseDTO, HttpStatus.OK);
     }
 
+    @PostMapping("/update-request")
+    public ResponseEntity<DriverProfileChangeResponseDTO> createChangeRequest(@RequestBody DriverProfileChangeRequestDTO request){
+            //treba pronaci drivera preko maila iz baze da bi id postavio id, sad je samo privremeno ovako
+        Driver foundDriver=new Driver();
+        foundDriver.setId(1L);
 
+        DriverProfileChangeRequest driverProfileChangeRequest = new DriverProfileChangeRequest();
+        driverProfileChangeRequest.setDriverId(foundDriver.getId());
+        driverProfileChangeRequest.setNewFirstName(request.getNewFirstName());
+        driverProfileChangeRequest.setNewLastName(request.getNewLastName());
+        driverProfileChangeRequest.setNewEmail(request.getNewEmail());
+        driverProfileChangeRequest.setNewPhoneNumber(request.getNewPhoneNumber());
+        driverProfileChangeRequest.setNewAddress(request.getNewAddress());
+        driverProfileChangeRequest.setNewProfilePicture(request.getNewProfilePicture());
+        driverProfileChangeRequest.setCreatedAt(LocalDateTime.now());
+        driverProfileChangeRequest.setStatus(DriverProfileChangeRequestStatus.PENDING);
+    // treba sacuvati ovo posle
+
+
+        DriverProfileChangeResponseDTO driverProfileChangeResponseDTO = new DriverProfileChangeResponseDTO(driverProfileChangeRequest);
+        return new ResponseEntity<>(driverProfileChangeResponseDTO, HttpStatus.CREATED);
+    }
 }
