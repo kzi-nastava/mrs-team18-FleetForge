3ec525c2e2122da150d00660de3c0e2658105af9
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2025-12-27 15:27:12 +0100

    feat(api): Add class for admin accept/reject vehicle information change

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/dto/admin/AdminVehicleChangeStatusResponseDTO.java
@@ -0,0 +1,14 @@
+package com.team18.FleetForge.dto.admin;
+
+import com.team18.FleetForge.model.enums.InformationChangeRequestStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminVehicleChangeStatusResponseDTO {
+    InformationChangeRequestStatus status;
+    Long vehicleId;
+}
