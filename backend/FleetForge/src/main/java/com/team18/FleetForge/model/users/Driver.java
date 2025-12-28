c5fd8925b4436c2be004ce3b0dfbeff33c6583aa
Milos Damjanovic <mdamjanovic2004@gmail.com>
2025-12-28 18:46:55 +0100

    Add attributes for driver

--- backend/FleetForge/src/main/java/com/team18/FleetForge/model/users/Driver.java
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/model/users/Driver.java
@@ -1,6 +1,7 @@
 package com.team18.FleetForge.model.users;
 
 
+import com.team18.FleetForge.model.GeoPoint;
 import com.team18.FleetForge.model.Vehicle;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,4 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Driver extends User {
     private Vehicle vehicle;
+    private boolean isAvailable;
+    private boolean isActive;
+    private GeoPoint currentLocation;
 }
