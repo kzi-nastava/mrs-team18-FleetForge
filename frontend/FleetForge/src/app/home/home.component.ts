b4427a6495f4d402572c1e1bbdc84f2c347c2c4c
Milos Damjanovic <mdamjanovic2004@gmail.com>
2026-01-11 15:54:36 +0100

    feat(web): Integrate vehicle display into home page

--- frontend/FleetForge/src/app/home/home.component.ts
+++ frontend/FleetForge/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MapComponent } from '../shared/map/map';
+import { VehicleService } from '../shared/services/vehicle.service';
+import { VehicleLocationDTO } from '../shared/models/vehicle.model';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,26 @@ import { MapComponent } from '../shared/map/map';
   styleUrl: './home.component.css',
   standalone: true
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  vehicles: VehicleLocationDTO[] = [];
+
+  constructor(private vehicleService: VehicleService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadVehicles();
+  }
+
+  loadVehicles(): void {
+    this.vehicleService.getActiveVehicles().subscribe({
+      next: (vehicles) => {
+        this.vehicles = [...vehicles]; 
+        this.cdr.detectChanges(); 
+        console.log('Loaded vehicles:', this.vehicles);
+        console.log('vehicles property after assignment:', this.vehicles);
+      },
+      error: (error) => {
+        console.error('Error loading vehicles:', error);
+      }
+    });
+  }
 }
--- frontend/FleetForge/src/app/shared/map/map.ts
+++ frontend/FleetForge/src/app/shared/map/map.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Input } from '@angular/core';
 import * as L from 'leaflet';
+import { VehicleLocationDTO } from '../models/vehicle.model';
 
 @Component({
   selector: 'app-map',
@@ -8,7 +9,22 @@ import * as L from 'leaflet';
   standalone: true
 })
 export class MapComponent implements AfterViewInit {
-  private map: any;
+  private _vehicles: VehicleLocationDTO[] = [];
+  
+  @Input() set vehicles(value: VehicleLocationDTO[]) {
+    this._vehicles = value;
+    console.log('Vehicles input changed:', value);
+    if (this.map) {
+      this.displayVehicles();
+    }
+  }
+  
+  get vehicles(): VehicleLocationDTO[] {
+    return this._vehicles;
+  }
+  
+  private map!: L.Map;
+  private vehicleMarkers: L.Marker[] = [];
 
   constructor() {}
 
@@ -28,26 +44,82 @@ export class MapComponent implements AfterViewInit {
       }
     );
     tiles.addTo(this.map);
+
+    this.map.on('zoom', () => {
+      this.updateMarkerSizes();
+    });
   }
 
-    ngAfterViewInit(): void {
-    let DefaultIcon = L.icon({
-      iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+  private updateMarkerSizes(): void {
+    const zoom = this.map.getZoom();
+    
+    const iconSize = 30 + (zoom - 10) * 3; 
+    
+    this.vehicleMarkers.forEach(marker => {
+      const icon = marker.getIcon() as L.DivIcon;
+      icon.options.iconSize = [iconSize, iconSize];
+      icon.options.iconAnchor = [iconSize / 2, iconSize / 2];
+      marker.setIcon(icon);
     });
+  }
 
-    L.Marker.prototype.options.icon = DefaultIcon;
+  ngAfterViewInit(): void {
     this.initMap();
+    
+    if (this.vehicles.length > 0) {
+      this.displayVehicles();
+    }
   }
 
-    registerOnClick(): void {
-    this.map.on('click', (e: any) => {
-      const coord = e.latlng;
-      const lat = coord.lat;
-      const lng = coord.lng;
-      console.log(
-        'You clicked the map at latitude: ' + lat + ' and longitude: ' + lng
-      );
-      new L.Marker([lat, lng]).addTo(this.map);
+
+  private displayVehicles(): void {
+    console.log('displayVehicles called. Map exists:', !!this.map, 'Vehicles count:', this.vehicles.length);
+    if (!this.map) return;
+
+    this.vehicleMarkers.forEach(marker => marker.remove());
+    this.vehicleMarkers = [];
+
+    this.vehicles.forEach(vehicle => {
+      console.log('Processing vehicle:', vehicle);
+      if (vehicle.isActive) {
+        const marker = this.createVehicleMarker(vehicle);
+        this.vehicleMarkers.push(marker);
+      }
     });
   }
+
+  private createVehicleMarker(vehicle: VehicleLocationDTO): L.Marker {
+    const { latitude, longitude } = vehicle.currentLocation;
+    const isAvailable = vehicle.isAvailable;
+    
+    const iconUrl = isAvailable ? '/green-car-icon.svg' : '/red-car-icon.svg';
+
+    const html = `
+      <img src="${iconUrl}" alt="vehicle" style="width: 40px; height: 40px;">
+    `;
+
+    const carIcon = L.divIcon({
+      html: html,
+      className: 'car-marker-icon',
+      iconSize: [40, 40],
+      iconAnchor: [20, 20],
+      popupAnchor: [0, -20],
+    });
+
+    const availabilityText = isAvailable ? 'Available' : 'Occupied';
+    const popupContent = `
+      <div style="font-family: var(--font-primary);">
+        <strong>${vehicle.model}</strong><br/>
+        Type: ${vehicle.vehicleType}<br/>
+        Status: <span font-weight: bold;">${availabilityText}</span>
+      </div>
+    `;
+
+    const marker = L.marker([latitude, longitude], { icon: carIcon })
+      .addTo(this.map)
+      .bindPopup(popupContent);
+
+    return marker;
+  }
+
 }
