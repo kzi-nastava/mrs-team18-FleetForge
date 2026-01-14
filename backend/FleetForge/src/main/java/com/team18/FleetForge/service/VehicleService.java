b3e64592f14ba8bc393150110616f4c61155239c
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-01-14 04:55:42 +0100

    feat(api): Add 2 functions in vehicle service for saving vehicles and finding vehicles by id

--- backend/FleetForge/src/main/java/com/team18/FleetForge/service/VehicleService.java
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/service/VehicleService.java
@@ -1,10 +1,13 @@
 package com.team18.FleetForge.service;
 
 import com.team18.FleetForge.dto.vehicle.VehicleLocationDTO;
+import com.team18.FleetForge.model.Vehicle;
 
 import java.util.List;
 
 public interface VehicleService {
 
     List<VehicleLocationDTO> getActiveVehicleLocations();
+    Vehicle findById(Long id);
+    void save(Vehicle vehicle);
 }
