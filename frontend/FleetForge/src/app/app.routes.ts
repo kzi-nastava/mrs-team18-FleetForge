f66d46bc44cac2e0bdedb5e5023b34cd29e170d4
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-01-15 20:54:32 +0100

    feat(web): Add routing to new page

--- frontend/FleetForge/src/app/app.routes.ts
+++ frontend/FleetForge/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { PasswordResetComponent } from './profiles/password-reset/password-reset
 import { DriverHistoryComponent } from './driver/driver-history/driver-history.component';
 import { AdminProfileComponent } from './profiles/admin-profile/admin-profile.component';
 import { DriverProfileChangesComponent } from './admin/driver-profile-changes/driver-profile-changes.component';
+import { RegisterDriverComponent } from './admin/register-driver/register-driver.component';
 
 export const routes: Routes = [
   /** ROUTES WITH NAVBAR */
@@ -27,7 +28,8 @@ export const routes: Routes = [
       { path: 'profile-driver', component: DriverProfileComponent },
       { path: 'profile-password-reset', component: PasswordResetComponent },
       { path: 'driver/ride-history', component: DriverHistoryComponent },
-      { path: 'admin/driver-changes', component: DriverProfileChangesComponent }
+      { path: 'admin/driver-changes', component: DriverProfileChangesComponent },
+      {path: 'admin/register-new-driver', component: RegisterDriverComponent}
     ]
   },
 
