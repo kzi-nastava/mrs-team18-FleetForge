45fde03c1275218b9eca7b5837a0ff6bee6cf4ee
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-01-17 03:01:37 +0100

    feat(web): Add functions that call backend for creating new driver and uploading profile picture

--- frontend/FleetForge/src/app/admin/register-driver/register-driver.component.ts
+++ frontend/FleetForge/src/app/admin/register-driver/register-driver.component.ts
@@ -3,6 +3,8 @@ import { Component, ElementRef, model, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { form } from '@angular/forms/signals';
 
+import { DriverCreateRequestDTO } from '../../shared/dtos/driver.dtos';
+import { RegisterDriverService } from '../service/register-driver/register-driver.service';
 @Component({
   selector: 'app-register-driver',
   imports: [CommonModule,ReactiveFormsModule],
@@ -10,6 +12,8 @@ import { form } from '@angular/forms/signals';
   styleUrl: './register-driver.component.css',
 })
 export class RegisterDriverComponent {
+  constructor(private registerDriverService: RegisterDriverService) {}
+  
   showVehicleForm: boolean = false;
 
   goToVehicleForm(): void {
@@ -23,24 +27,26 @@ export class RegisterDriverComponent {
     this.showVehicleForm = false;
   }
 @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  formData = new FormData();
   imageUrl: string = 'blank_profile.webp';
   openFilePicker(): void {
     this.fileInput.nativeElement.click();
   }
   onFileSelected(event: Event): void {
+    if(this.formData.has('file')){
+      this.formData.delete('file');
+    }
     const input = event.target as HTMLInputElement;
     if (!input.files || input.files.length === 0) return;
 
     const file = input.files[0];
-    // ovde treba sacuvati sliku na server ili u bazu podataka
-    // za sada samo prikazujemo izabranu sliku
-    // pozivom servisa this.userService.uploadProfilePicture(file);
-  this.imageUrl = URL.createObjectURL(file);
-  this.registerDriverForm.patchValue({
+    this.formData.append('file', file);
+     this.imageUrl = URL.createObjectURL(file);
+    this.registerDriverForm.patchValue({
     driverInfo: {
-      profilePicture: file.name
+      profilePicture: file.name 
     }
-  });
+  } );
   }
 registerDriverForm = new FormGroup({
   driverInfo: new FormGroup({
@@ -72,15 +78,37 @@ registerDriver(): void {
       this.registerDriverForm.markAllAsTouched();
       return;
     }
+    const request: DriverCreateRequestDTO = { 
+  firstName: this.driverInfoFormGroup.get('firstName')?.value!,
+  lastName: this.driverInfoFormGroup.get('lastName')?.value!,
+  email: this.driverInfoFormGroup.get('email')?.value!,
+  phoneNumber: this.driverInfoFormGroup.get('phoneNumber')?.value!,
+  address: this.driverInfoFormGroup.get('address')?.value!,
+  vehicle: {
+    model: this.vehicleInfo.get('model')?.value!,
+    type: this.vehicleInfo.get('type')?.value!,
+    registrationNumber: this.vehicleInfo.get('registrationNumber')?.value!,
+    space: this.vehicleInfo.get('space')?.value!,
+    babySeat: this.vehicleInfo.get('babySeat')?.value!,
+    petFriendly: this.vehicleInfo.get('petFriendly')?.value!
+  }
+    };
 
-    console.log('Form Data:', this.registerDriverForm.value);
+  this.registerDriverService.registerDriver(request).subscribe({
+      next: (response) => {
+        this.registerDriverService.uploadProfilePicture(this.formData, response.driver.id).subscribe();
+        alert('Driver successfully registered!');
+      },  
+      error: (error) => {
+        console.error('Error registering driver:', error);
+      }
+    });
     
    
-alert('Driver successfully registered!');
+
   this.resetForm(); 
 }
 
-// Nova metoda za reset
 resetForm(): void {
   this.registerDriverForm.reset({
     driverInfo: {
