fe47c88ecb6e411695bb530ec7493afe673b13c5
Milos Damjanovic <mdamjanovic2004@gmail.com>
2026-01-26 13:20:47 +0100

    feat(web): Create driver and passenger current ride services targeting REST endpoints

---
+++ frontend/FleetForge/src/app/driver/service/driver-current-ride/driver-current-ride.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DriverLocationUpdateRequestDTO, DriverLocationUpdateResponseDTO } from '../../../shared/dtos/driver-location-update.dto';
+import { RideTrackingDTO } from '../../../shared/dtos/ride-tracking.dtos';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DriverCurrentRide {
+  private apiUrl = 'http://localhost:8080/api/rides';
+
+  constructor(private http: HttpClient) {}
+
+  updateLocation(request: DriverLocationUpdateRequestDTO): Observable<DriverLocationUpdateResponseDTO> {
+    return this.http.post<DriverLocationUpdateResponseDTO>(
+      `${this.apiUrl}/driver-location-update`,
+      request
+    );
+  }
+
+  getActiveTracking(): Observable<RideTrackingDTO> {
+    return this.http.get<RideTrackingDTO>(`${this.apiUrl}/active-tracking`);
+  }
+}
