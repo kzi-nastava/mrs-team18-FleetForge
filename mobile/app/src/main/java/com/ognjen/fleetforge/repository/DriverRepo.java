6af0115e9ce948119628d3aa3159303c183d8729
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-02-04 14:50:18 +0100

    feat(mobile): Add repositories for admin and driver that call services

---
+++ mobile/app/src/main/java/com/ognjen/fleetforge/repository/DriverRepo.java
@@ -0,0 +1,154 @@
+package com.ognjen.fleetforge.repository;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.ognjen.fleetforge.api.DriverService;
+import com.ognjen.fleetforge.api.RetrofitClient;
+import com.ognjen.fleetforge.dtos.common.PasswordChangeRequestDTO;
+import com.ognjen.fleetforge.dtos.driver.DriverGetResponseDTO;
+import com.ognjen.fleetforge.dtos.driver.DriverProfileChangeRequestDTO;
+import com.ognjen.fleetforge.dtos.driver.DriverProfileChangeResponseDTO;
+import com.ognjen.fleetforge.dtos.vehicle.VehicleInformationChangeRequestDTO;
+import com.ognjen.fleetforge.dtos.vehicle.VehicleInformationChangeResponseDTO;
+import com.ognjen.fleetforge.utils.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DriverRepo {
+    private DriverService service;
+
+    public DriverRepo(){
+        service= RetrofitClient.getInstance().getDriverService();
+    }
+
+    public LiveData<DriverGetResponseDTO> getLoggedDriver(){
+        MutableLiveData<DriverGetResponseDTO> data= new MutableLiveData<>();
+
+        service.getLoggedDriver().enqueue(new Callback<DriverGetResponseDTO>() {
+            @Override
+            public void onResponse(Call<DriverGetResponseDTO> call, Response<DriverGetResponseDTO> response) {
+                if(response.isSuccessful()){
+                    data.setValue(response.body());
+                }else{
+                    data.setValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DriverGetResponseDTO> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+        return data;
+    }
+
+    public LiveData<DriverProfileChangeResponseDTO> createDriverChangeRequest(DriverProfileChangeRequestDTO request){
+        MutableLiveData<DriverProfileChangeResponseDTO> data= new MutableLiveData<>();
+
+        service.createDriverChangeRequest(request).enqueue(new Callback<DriverProfileChangeResponseDTO>() {
+            @Override
+            public void onResponse(Call<DriverProfileChangeResponseDTO> call, Response<DriverProfileChangeResponseDTO> response) {
+                if(response.isSuccessful()){
+                    data.setValue(response.body());
+                }else{
+                    data.setValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DriverProfileChangeResponseDTO> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+        return data;
+    }
+
+    public LiveData<VehicleInformationChangeResponseDTO> createVehicleChangeRequest(VehicleInformationChangeRequestDTO request){
+        MutableLiveData<VehicleInformationChangeResponseDTO> data= new MutableLiveData<>();
+
+        service.createVehicleChangeRequest(request).enqueue(new Callback<VehicleInformationChangeResponseDTO>() {
+            @Override
+            public void onResponse(Call<VehicleInformationChangeResponseDTO> call, Response<VehicleInformationChangeResponseDTO> response) {
+                if(response.isSuccessful()){
+                    data.setValue(response.body());
+                }else{
+                    data.setValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<VehicleInformationChangeResponseDTO> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+        return data;
+    }
+    public LiveData<Boolean> uploadProfilePic(Context context, Uri imageUri) throws IOException {
+        MutableLiveData<Boolean> result= new MutableLiveData<Boolean>();
+        MultipartBody.Part imagePart=null;
+        try {
+            File file = FileUtil.getFileFromUri(context, imageUri);
+            String mimeType = context.getContentResolver().getType(imageUri);
+            if (mimeType == null){
+                result.setValue(false);
+                return result;
+            }
+            RequestBody requestBody = RequestBody.create(MediaType.parse(mimeType), file);
+            imagePart = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+
+
+            service.uploadProfilePictureCurrentUser(imagePart).enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
+                    if(response.isSuccessful()){
+                        result.setValue(true);
+                    }else{
+                        result.setValue(false);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+                    result.setValue(false);
+                }
+            });
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            result.setValue(false);
+        }
+        return result;
+    }
+    public LiveData<Boolean> changePassword(PasswordChangeRequestDTO request){
+        MutableLiveData<Boolean> data= new MutableLiveData<>();
+
+        service.changePassword(request).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()){
+                    data.setValue(true);
+                }else{
+                    data.setValue(false);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                data.setValue(true);
+            }
+        });
+        return data;
+    }
+}
