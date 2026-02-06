739268ffaec3c0630ce21f042d983d0cf3febc2f
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-02-06 01:24:11 +0100

    feat(mobile): Add needed dto classes

---
+++ mobile/app/src/main/java/com/ognjen/fleetforge/dtos/driver/DriverCreateResponseDTO.java
@@ -0,0 +1,13 @@
+package com.ognjen.fleetforge.dtos.driver;
+
+public class DriverCreateResponseDTO {
+    private Long driverId;
+
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+}
