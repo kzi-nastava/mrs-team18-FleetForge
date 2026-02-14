4e1111963f393cb74243d0a63fecf7b891b1d5bf
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-02-14 18:59:13 +0100

    test(frontend): Add config for jasmine tests instead of vitest

--- frontend/FleetForge/src/app/passenger/dashboard/dashboard.component.spec.ts
+++ frontend/FleetForge/src/app/passenger/dashboard/dashboard.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { PassengerDashboardComponent } from './dashboard.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('PassengerDashboardComponent', () => {
+  let component: PassengerDashboardComponent;
+  let fixture: ComponentFixture<PassengerDashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [PassengerDashboardComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(PassengerDashboardComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
