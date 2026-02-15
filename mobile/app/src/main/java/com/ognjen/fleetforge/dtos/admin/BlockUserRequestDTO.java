b935515dc08d22b7bc7427a35514092f96c17c40
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-02-15 19:56:51 +0100

    feat(mobile): Add needed dto classes

---
+++ mobile/app/src/main/java/com/ognjen/fleetforge/dtos/admin/BlockUserRequestDTO.java
@@ -0,0 +1,13 @@
+package com.ognjen.fleetforge.dtos.admin;
+
+public class BlockUserRequestDTO {
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    private String reason;
+}
---
+++ mobile/app/src/main/java/com/ognjen/fleetforge/dtos/user/UserInformationDTO.java
@@ -0,0 +1,82 @@
+package com.ognjen.fleetforge.dtos.user;
+
+public class UserInformationDTO {
+    private Long id;
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String profilePicture;
+
+    private boolean blocked;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+}
