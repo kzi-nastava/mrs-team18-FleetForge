87ebec41c05d2b50020374011e3c5907c7ced698
Milos Damjanovic <mdamjanovic2004@gmail.com>
2026-02-16 20:21:05 +0100

    feat(mobile): Add endpoint for active tracking

--- mobile/app/src/main/java/com/ognjen/fleetforge/api/RideService.java
+++ mobile/app/src/main/java/com/ognjen/fleetforge/api/RideService.java
@@ -1,12 +1,16 @@
 package com.ognjen.fleetforge.api;
 
+import com.ognjen.fleetforge.dtos.driver.DriverLocationUpdateRequestDTO;
+import com.ognjen.fleetforge.dtos.driver.DriverLocationUpdateResponseDTO;
 import com.ognjen.fleetforge.dtos.ride.FinishRideResponseDTO;
 import com.ognjen.fleetforge.dtos.ride.RideCreateRequestDTO;
 import com.ognjen.fleetforge.dtos.ride.RideCreateResponseDTO;
 import com.ognjen.fleetforge.dtos.ride.RideStartResponseDTO;
+import com.ognjen.fleetforge.dtos.ride.RideTrackingDTO;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -20,4 +24,12 @@ public interface RideService {
 
     @PUT("/api/rides/{rideId}/finish")
     Call<FinishRideResponseDTO> finishRide(@Path("rideId") Long rideId);
+
+    @GET("api/rides/active-tracking")
+    Call<RideTrackingDTO> getActiveRideTracking();
+
+    @POST("api/rides/driver-location-update")
+    Call<DriverLocationUpdateResponseDTO> updateDriverLocation(
+            @Body DriverLocationUpdateRequestDTO request
+    );
 }
