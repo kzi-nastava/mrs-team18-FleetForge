509185a1730f03177c8c6d4621298ab3c2eb39dd
Vukasin Vitomirović <vitomirovic.sv46.2023@uns.ac.rs>
2026-02-16 13:51:33 +0100

    feat(api): Add needed dto classes

---
+++ backend/FleetForge/src/main/java/com/team18/FleetForge/dto/ride/reports/UserDataReportResponseDTO.java
@@ -0,0 +1,17 @@
+package com.team18.FleetForge.dto.ride.reports;
+
+import com.team18.FleetForge.model.ride.Ride;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDataReportResponseDTO {
+    private Map<LocalDate, List<Ride>> dataByDay;
+}
